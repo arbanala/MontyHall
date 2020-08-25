3281ed666fc03cfc2ddbda124688dab6982e712c
arbanala <55905751+arbanala@users.noreply.github.com>
2020-08-25 15:41:27 -0400

    Update MontyHall.py

--- MontyHall.py
+++ MontyHall.py
@@ -2,21 +2,25 @@ import random
 
 ans = random.randint(1, 3)
 choice = random.randint(1, 3)
+print("You have chosen door %d" % choice)
 opendoor = 0
 change = random.randint(1, 2)
 for x in range(1, 3):
-    if x != ans & x != choice
-        print("%d does not have the prize. Do you want to change your choice?", x)
+    if x != ans and x != choice:
+        print("%d does not have the prize. Do you want to change your choice?" % x)
         opendoor = x
         break
         
-if change == 2
+if change == 2:
     for x in range(1, 3):
-        if x != opendoor & x != choice
+        if x != opendoor and x != choice:
             choice = x
+            print("You have changed your door to door %d" % choice)
             break
+else:
+    print("You have not changed your choice.")
  
-if choice == ans
+if choice == ans:
     print("We have a winner!")
-else
-    print("We don't have a winner. Better luck next time!")
+else:
+    print("The prize was in door %d. Better luck next time!" % ans)
